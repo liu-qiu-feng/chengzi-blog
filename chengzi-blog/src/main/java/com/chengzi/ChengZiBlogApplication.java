63332570975817cbb2ff0da78ebcb2b658eb6b06
liu-qiu-feng <3082859219@qq.com>
2022-10-23 00:01:31 +0800

    Blog

---
+++ chengzi-blog/src/main/java/com/chengzi/ChengZiBlogApplication.java
@@ -0,0 +1,11 @@
+package com.chengzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChengZiBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChengZiBlogApplication.class,args);
+    }
+}
